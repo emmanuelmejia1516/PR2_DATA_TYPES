4abef7e180b1372cbd45e7081cc56b69574f7839
emmanuelmejia1516 <a.23308051281197@cbtis128.edu.mx>
2024-09-23 20:28:52 -0600

    Add files via upload

---
+++ pr2_data_numbers.py
@@ -0,0 +1,14 @@
+print("")
+print("mejia suarez emmanuel alexander:mi practica imprime el tipo de cada variable")
+print("")
+# Lista de frutas
+x = ["manzana", "banana", "uva"]  # Las listas son mutables.
+# Tupla de frutas
+y = ("manzana", "banana", "uva")  # Las tuplas son inmutables.
+# Tupla con un solo elemento (nota la coma)
+z = ("manzana")  
+
+# Imprimir el tipo de cada variable
+print(type(x))  # Imprimirá: <class 'list'>
+print(type(y))  # Imprimirá: <class 'tuple'>
+print(type(z))  # Imprimirá: <class 'str'>
